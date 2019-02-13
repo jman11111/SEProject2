e9fa145bfa170830a070413fb0f53d4a8a6e6a99
jack <jbroncato@luc.edu>
2019-02-12 19:14:03 -0600

    new look, still needs some better spacing

--- src/main/java/GUIMain.java
+++ src/main/java/GUIMain.java
@@ -10,26 +10,53 @@ public class GUIMain {
         JFrame frame = new JFrame("Twitter Search");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
-        frame.setPreferredSize(new Dimension(300, 200));
+        frame.setPreferredSize(new Dimension(400, 300));
 
+        /* Panel for putting in tweet to be analyzed*/
         JPanel tweetpnl = new JPanel();
         tweetpnl.setLayout(new BoxLayout(tweetpnl, BoxLayout.PAGE_AXIS));
         tweetpnl.setBorder(BorderFactory.createEmptyBorder(0,10,10,10));
 
-        //textfield for tweets
+        JLabel hehe = new JLabel("Tweet something:   ");
         JTextField tweetbar = new JTextField();
 
-        JPanel tweetbtns = new JPanel();
-        tweetbtns.setLayout(new BoxLayout(tweetbtns, BoxLayout.LINE_AXIS));
-        tweetbtns.setBorder(BorderFactory.createEmptyBorder(0,10,10,10));
+        // Panel for dsiplaying similar tweets to user
+        JPanel similartweets = new JPanel();
+        similartweets.setLayout(new BoxLayout(similartweets, BoxLayout.PAGE_AXIS));
+
+        JLabel descriptor = new JLabel("Similar Tweets:     ");
+        JTextPane tweetsfound = new JTextPane();
+
+        // Panel for options when trying to find similar tweets
+        JPanel options = new JPanel();
+        options.setLayout(new BoxLayout(options, BoxLayout.PAGE_AXIS));
+
+        JLabel optiondesc = new JLabel("Filter by:  ");
+        JButton mentionbutton = new JButton("Mention");
+        JButton topicbutton = new JButton("Topic");
+        JButton urlbutton = new JButton("URL");
+
+        // Panel for button that accepts tweet in textbox
+        JPanel tweetbtn = new JPanel();
+        tweetbtn.setLayout(new BoxLayout(tweetbtn, BoxLayout.LINE_AXIS));
+        tweetbtn.setBorder(BorderFactory.createEmptyBorder(0,10,10,10));
 
         JButton tweetbutton = new JButton("Get similar tweets");
 
+        tweetpnl.add(hehe);
         tweetpnl.add(tweetbar);
-        tweetbtns.add(tweetbutton);
+        options.add(optiondesc);
+        options.add(mentionbutton);
+        options.add(topicbutton);
+        options.add(urlbutton);
+        similartweets.add(descriptor);
+        similartweets.add(tweetsfound);
+        tweetbtn.add(tweetbutton);
         Container contentPane = frame.getContentPane();
-        contentPane.add(tweetpnl, BorderLayout.CENTER);
-        contentPane.add(tweetbtns, BorderLayout.PAGE_END);
+        contentPane.add(tweetpnl, BorderLayout.LINE_START);
+        contentPane.add(options, BorderLayout.CENTER);
+        contentPane.add(similartweets, BorderLayout.LINE_END);
+        contentPane.add(tweetbtn, BorderLayout.PAGE_END);
         frame.pack();
         frame.setVisible(true);
     }
