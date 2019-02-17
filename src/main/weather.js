0abc7778e2cae6138622305515841b95e6d28ffb
jnarowski1 <jnarowski1@luc.edu>
2019-02-17 13:16:53 -0600

    New outline of Weather post 2/17 meeting

--- src/main/weather.js
+++ src/main/weather.js
@@ -1,6 +1,6 @@
-//todo 1) take a found tweet
-//todo 2) check if the tweet has coordinates or place
-//todo 3a) convert that twitter place id to the yahoo place id
-//todo 3b) or if it has coordinates skip this step and use those directly in weather api
-//todo 4) use that place in the yahoo weather api to find the weather at that place
-//todo 5) display that weather
+//todo 1) take a location
+//todo 2) convert that location to work with the yahoo weather api
+//todo 3) use that place in the yahoo weather api to find the weather at that place
+//todo 4) return two different items
+//todo           1) Weather type
+//todo           2) Temperature
