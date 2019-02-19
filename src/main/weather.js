640eb2d2fb0f543f019d512551c69ed9890a4d5d
jnarowski1 <jnarowski1@luc.edu>
2019-02-19 14:48:53 -0600

    Yahoo OAuth template documentation for accessing Weather API

--- src/main/weather.js
+++ src/main/weather.js
@@ -4,3 +4,31 @@
 //todo 4) return two different items
 //todo           1) Weather type
 //todo           2) Temperature
+
+var OAuth = require('oauth');
+var header = {
+    "Yahoo-App-Id": "your-app-id"
+};
+var request = new OAuth.OAuth(
+    null,
+    null,
+    'your-consumer-key',
+    'your-consumer-secret',
+    '1.0',
+    null,
+    'HMAC-SHA1',
+    null,
+    header
+);
+request.get(
+    'https://weather-ydn-yql.media.yahoo.com/forecastrss?location=sunnyvale,ca&format=json',
+    null,
+    null,
+    function (err, data, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(data)
+        }
+    }
+);
