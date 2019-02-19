289a17ddd1667b697e5e1361f19be39603632292
Jack <jbroncato@luc.edu>
2019-02-19 17:08:07 -0600

    apis work now i can send headers, turns out there is a twitter library to use

--- src/main/server.js
+++ src/main/server.js
@@ -1,58 +1,31 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const https = require('https');
+const axios = require('axios')
+var Twitter = require('twitter-node-client').Twitter;
 
-let data = '';
-
-var options = {
-  host: "developer.api.autodesk.com",
-  path: "/oss/v1/buckets",
-  method: "POST",
-  headers: {
-      "Content-Type": "application/json",
-      "Authorization": "Bearer token"
-  }
+var error = function (err, response, body) {
+  console.log('ERROR [%s]', err);
 };
+var success = function (data) {
+  console.log('Data [%s]', data);
+};
+    
+var twitter = new Twitter();
 
-/*var post_options = {
-  host: 'api.github.com',
-  path: '/jman11111/repos',
-  port: '8000',
-  method: 'POST',
-  headers: {
-    'User-Agent': 'jman11111'
-  }
-}*/
-app.get('/', (request, response) => {
-  
-
-  https.request(options, (resp) => {
-  
-    // A chunk of data has been recieved.
-    resp.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-      console.log('data sent');
-      response.send(data)
-      data = '';
-    });
-
-  }).on("error", (err) => {
-  console.log("Error: " + err.message);
-  });
-  
-})
-
-app.get('/dev', (request, response) => {
-    response.send('You are on /dev')
-})
-
-app.get('/json', (request, response) => {
-  response.send()
+app.get('/', function (req, res) {
+  axios.get('https://api.github.com/users/jman11111/repos', {
+    headers: {
+      'User-Agent': 'jman11111' 
+    }
+   })
+    .then(resp => {
+      console.log(resp.data);
+      res.send(resp.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
 
 app.listen(port, (err) => {
