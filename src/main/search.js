6320c650b3da55f108400064da8b2a2b34db0cfb
Jean Luc <jl@conntech.com>
2019-02-19 21:31:26 -0600

    depenecies for twitter api added

--- src/main/search.js
+++ src/main/search.js
@@ -1,16 +1,15 @@
-var tweet = require('twitter'),
-    twitter = new tweet({
-        consumer_key: 'sCcnjWyY5I09rj9rNZRsjntZN',
-        consumer_secret: 'yBcwz07LVUTNwL4ZKFUWo4bTHQw94JTHIjJiuypSsB1AvHGNLB',
-        access_token: '1097228851397173248-eE0byHpuvAxRwvYnSKzWTB0JnmtqHb',
-        access_secret: 'kR0IcRZ6KgfH5FvHlbJxwrXkfPUXhYvMVoxGn3U2KlrPx'
+var twit = require('twitter'),
+    twitter = new twit({
+        consumer_key: 'YGfbBkpdB3LzsDbOQq34I8bOK',
+        consumer_secret: 'FhRgGKY5oWX2CDJ3JrsLkdmIofE3BgsSMugNllZAeBiCt5lAqJ',
+        access_token: '1097228851397173248-5HLGeymTaxe7VtoCDq6GhSGnHDiSQf',
+        access_secret: 'fMpt9u8chF4OdjV8vlgp1gP2CcFKM02HHVXiTsMIF4C2n'
     });
 
 var count = 0,
     util = require ('util');
 
-
-twitter.stream('filter',{track: 'thanks'}, function(stream){
+twitter.stream('filter',{track: 'love'}, function(stream){
 
     stream.on('data',function(data){
         console.log(util.inspect(data));
