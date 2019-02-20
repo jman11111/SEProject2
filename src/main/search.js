f5ee460eb91eadfba7ba55a9efaa407aebf38392
Jean Luc <jl@conntech.com>
2019-02-19 22:16:44 -0600

    requestify modules added for get, attempt at fetcing data

--- src/main/search.js
+++ src/main/search.js
@@ -1,5 +1,6 @@
 var twit = require('twitter'),
     twitter = new twit({
+
         consumer_key: 'YGfbBkpdB3LzsDbOQq34I8bOK',
         consumer_secret: 'FhRgGKY5oWX2CDJ3JrsLkdmIofE3BgsSMugNllZAeBiCt5lAqJ',
         access_token: '1097228851397173248-5HLGeymTaxe7VtoCDq6GhSGnHDiSQf',
@@ -9,6 +10,12 @@ var twit = require('twitter'),
 var count = 0,
     util = require ('util');
 
+var requestify = require('requestify');
+requestify.get('https://stream.twitter.com/1.1/statuses/filter.json').then(function(response) {
+  // Get the response body (JSON parsed or jQuery object for XMLs)
+  response.getBody();
+//get request https://stream.twitter.com/1.1/statuses/filter.json
+
 twitter.stream('filter',{track: 'love'}, function(stream){
 
     stream.on('data',function(data){
@@ -17,4 +24,6 @@ twitter.stream('filter',{track: 'love'}, function(stream){
         stream.destroy();
         process.exit(0);
     });
-});    
+   
+});
+});
