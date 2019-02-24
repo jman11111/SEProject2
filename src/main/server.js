9ec86894b2c5b1aed3a2352621ba9dcb1c4b06a2
Jean Luc <jl@conntech.com>
2019-02-24 17:05:32 -0600

    cleaned up, need to figure out await

--- src/main/server.js
+++ src/main/server.js
@@ -3,12 +3,12 @@ const app = express()
 const port = 3000
 const axios = require('axios')
 var Twitter = require('twitter-node-client').Twitter;
-
+var tweetdata;
 var error = function (err, response, body) {
   console.log('ERROR [%s]', err);
 };
-var success = function (data) {
-  console.log('Data [%s]', data);
+var success =  async function (data) {
+  return tweetdata = data;
 };
 
 var config = {
@@ -21,21 +21,9 @@ var config = {
 
 var twitter = new Twitter(config);
 
-twitter.getSearch({'q':'#haiku','count': 10}, error, success);
-
-app.get('/', function (req, res) {
-  axios.get('https://api.github.com/users/jman11111/repos', {
-    headers: {
-      'User-Agent': 'jman11111' 
-    }
-   })
-    .then(resp => {
-      console.log(resp.data);
-      res.send(resp.data);
-    })
-    .catch(err => {
-      console.log(err);
-    })
+app.get('/', async function (req, res) {
+     await twitter.getSearch({'q':'#love','count': 10}, error, success);
+      res.send(tweetdata);
 })
 
 app.listen(port, (err) => {
