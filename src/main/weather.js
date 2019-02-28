8e07194c1f2cc18b7217f48e95b8b068ab9b5f71
jnarowski1 <jnarowski1@luc.edu>
2019-02-27 19:48:30 -0600

    Weather Server! w/ post

--- src/main/weather.js
+++ src/main/weather.js
@@ -1,32 +1,40 @@
-//todo 1) take a location
-//todo 2) convert that location to work with the yahoo weather api
-//todo 3) use that place in the yahoo weather api to find the weather at that place
-//todo 4) return two different items
-//todo           1) Weather type
-//todo           2) Temperature
+const express = require('express')
+const app = express()
+const port = 3001
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log(`weather server is listening on ${port}`)
+})
 
 var OAuth = require('oauth');
 var header = {
     "Yahoo-App-Id": "9DNtWM4e"
 };
-var request = new OAuth.OAuth(
-    null,
-    null,
-    'dj0yJmk9RkVhUE1DSWw5RVMyJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTcy',
-    'a0fe2b1bea9e9515f811bddd3cb3ff7a4cd13878',
-    '1.0',
-    null,
-    'HMAC-SHA1',
-    null,
-    header
-);
 
-var location = 'chicago';
+app.post('/weather', function (req, res) {
+    var location =req.body.loc; //from front end version
+    //var location = 'loc'; //manual version
 
-var output;
+    var request = new OAuth.OAuth(
+        null,
+        null,
+        'dj0yJmk9RkVhUE1DSWw5RVMyJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTcy',
+        'a0fe2b1bea9e9515f811bddd3cb3ff7a4cd13878',
+        '1.0',
+        null,
+        'HMAC-SHA1',
+        null,
+        header
+    );
+
+    var output;
 
     request.get(
-        'https://weather-ydn-yql.media.yahoo.com/forecastrss?location='+location+'&format=json',//do I need to make this string conform better
+        'https://weather-ydn-yql.media.yahoo.com/forecastrss?location=' + location + '&format=json',
         null,
         null,
         function (err, data, result) {
@@ -36,5 +44,5 @@ var output;
                 //output = data;
                 console.log(data);
             }
-        }
-    );
+        });
+})
